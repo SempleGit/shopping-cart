c0b8fd76579c94492e04b62bbfdde11dfea8b614
Daniel Semple <dsemple.isu@gmail.com>
2020-12-19 07:36:22 -0600

    details

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,10 @@ const Home = () => {
   return (
     <div>
       <h1>Shopping Home</h1>
+      {/* 1. create item list
+          2. add item to cart
+          3. remove item from cart
+          4. display items on home. */}
     </div>
   )
 }
