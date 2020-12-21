4d888fbf3c05a639e4fb05701f2c6cc00735d050
Daniel Semple <dsemple.isu@gmail.com>
2020-12-21 12:16:31 -0600

    added display of the shopping cart state. added ability remove items from cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,45 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from 'react';
 
-const Cart = () => {
+const Cart = (props) => {
+
+  useEffect(() => {
+    document.addEventListener("click", removeFromCart);
+    return () => {
+      document.removeEventListener("click", removeFromCart);
+    };
+  });
+
+  const cartDisplay = () => {
+    const shoppingCart = {...props.cartItems};
+    const items = Object.keys(shoppingCart);
+    const display = items.map(item => (
+      <li key={item} id={item}>Item: {item} Quantity: {shoppingCart[item]}</li>
+    ));
+    return display;
+  }
+
+  const removeFromCart = (e) => {
+    if (e.target.id === '' || e.target.id === 'root') return;   
+    const itemToRemove = e.target.id;
+    props.setCart((items) => {
+      let newItems = {...items};
+      if (newItems[itemToRemove] > 1)  {
+        newItems = {...newItems, [itemToRemove]: --newItems[itemToRemove] };
+       } else {
+         delete newItems[itemToRemove];
+       }
+       return newItems
+    });
+  }
+
+  cartDisplay();
 
   return (
     <div>
       <h1>Shopping Cart</h1>
+      <ul>
+        {cartDisplay()}
+      </ul>
     </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,6 @@ import React, { useEffect } from 'react';
 const Home = (props) => {
   useEffect(() => {
     document.addEventListener("click", addToCart);
-    console.log(props.cartItems, 'prop');
     return () => {
       document.removeEventListener("click", addToCart);
     };
